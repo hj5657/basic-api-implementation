1c5f09cf58eadbf495ca507ecfa2402f65a8d079
hejie <565703319@qq.com>
2020-09-15 21:46:26 +0800

    [hejie]: fix code format

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -9,31 +9,31 @@ import java.util.List;
 
 @RestController
 public class RsController {
-  private List<RsEvent> rsList = initAddRsEvent();
+    private List<RsEvent> rsList = initAddRsEvent();
 
-  private List<RsEvent> initAddRsEvent() {
-    List<RsEvent> list = new ArrayList<>();
-    list.add(new RsEvent("第一条事件","无标签"));
-    list.add(new RsEvent("第二条事件","无标签"));
-    list.add(new RsEvent("第三条事件","无标签"));
-    return list;
-  }
-
-  @GetMapping("/rs/{index}")
-  public RsEvent getOneRs(@PathVariable Integer index){
-    return rsList.get(index-1);
-  }
-
-  @GetMapping("/rs/list")
-  public List<RsEvent> getRsBetween(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end){
-    if (start==null&&end==null){
-      return rsList;
+    private List<RsEvent> initAddRsEvent() {
+        List<RsEvent> list = new ArrayList<>();
+        list.add(new RsEvent("第一条事件", "无标签"));
+        list.add(new RsEvent("第二条事件", "无标签"));
+        list.add(new RsEvent("第三条事件", "无标签"));
+        return list;
     }
-    return rsList.subList(start-1,end);
-  }
 
-  @PostMapping("/rs/event")
-  public void addRsEvent(@RequestBody RsEvent rsEvent){
-    rsList.add(rsEvent);
-  }
+    @GetMapping("/rs/{index}")
+    public RsEvent getOneRs(@PathVariable Integer index) {
+        return rsList.get(index - 1);
+    }
+
+    @GetMapping("/rs/list")
+    public List<RsEvent> getRsBetween(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+        if (start == null && end == null) {
+            return rsList;
+        }
+        return rsList.subList(start - 1, end);
+    }
+
+    @PostMapping("/rs/event")
+    public void addRsEvent(@RequestBody RsEvent rsEvent) {
+        rsList.add(rsEvent);
+    }
 }
