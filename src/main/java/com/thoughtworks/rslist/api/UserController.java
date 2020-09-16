2f8bd47d6d40a9ec10e7fe1602dc881d78026d9a
hejie <565703319@qq.com>
2020-09-16 19:49:28 +0800

    [hejie]: finish valid test

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,7 @@ import java.util.List;
 public class UserController {
     List<User> userList = new ArrayList<>();
     @PostMapping("/user")
-    public void addUser(@RequestBody User user){
+    public void addUser(@RequestBody @Valid User user){
         userList.add(user);
     }
 
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -2,7 +2,10 @@ package com.thoughtworks.rslist.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,25 +23,77 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Order(1)
     @Test
     public void should_register_user() throws Exception {
-        User user=new User("hejie",22,"male","hj@c","13599999999");
-        ObjectMapper objectMapper =new ObjectMapper();
+        User user = new User("hejie", 22, "male", "hj@c", "13599999999");
+        ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(user);
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         mockMvc.perform(get("/user"))
-                .andExpect(jsonPath("$",hasSize(1)))
-                .andExpect(jsonPath("$[0].userName",is("hejie")))
-                .andExpect(jsonPath("$[0].age",is(22)))
-                .andExpect(jsonPath("$[0].gender",is("male")))
-                .andExpect(jsonPath("$[0].email",is("hj@c")))
-                .andExpect(jsonPath("$[0].phone",is("13599999999")))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].userName", is("hejie")))
+                .andExpect(jsonPath("$[0].age", is(22)))
+                .andExpect(jsonPath("$[0].gender", is("male")))
+                .andExpect(jsonPath("$[0].email", is("hj@c")))
+                .andExpect(jsonPath("$[0].phone", is("13599999999")))
                 .andExpect(status().isOk());
+    }
+
+    @Order(2)
+    @Test
+    public void name_should_not_more_than_8_length() throws Exception {
+        User user = new User("hejiehhhh", 22, "male", "hj@c", "13599999999");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Order(3)
+    @Test
+    public void gender_should_not_null() throws Exception {
+        User user = new User("hejie", 22, null, "hj@c", "13599999999");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Order(4)
+    @Test
+    public void age_should_between_18_to_100() throws Exception {
+        User user = new User("hejie", 122, "male", "hj@c", "13599999999");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Order(5)
+    @Test
+    public void email_should_be_format() throws Exception {
+        User user = new User("hejie", 22, "male", "hehehe", "13599999999");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Order(6)
+    @Test
+    public void phone_should_start_1_total_11_number() throws Exception {
+        User user = new User("hejie", 22, "male", "hj@c", "135999999999");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
