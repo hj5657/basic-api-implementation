252dbda201406fa0d39e225b8b13ab6eff937d2f
hejie <565703319@qq.com>
2020-09-16 15:45:45 +0800

    [hejie]: finish addUser and getUserList

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +12,16 @@ import java.util.List;
 /**
  * Create by 木水 on 2020/9/16.
  */
+@RestController
 public class UserController {
     List<User> userList = new ArrayList<>();
+    @PostMapping("/user")
+    public void addUser(@RequestBody User user){
+        userList.add(user);
+    }
+
+    @GetMapping("/user")
+    public List<User> getUserList(){
+        return userList;
+    }
 }
