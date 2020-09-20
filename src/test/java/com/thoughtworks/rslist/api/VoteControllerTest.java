d61356713ffaf7d4eb9b7e4b83981f2cf6a67585
hejie <565703319@qq.com>
2020-09-20 13:00:49 +0800

    [hejie]: vote controller and test init

---
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class VoteControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @BeforeEach
+    void setUp() {
+    }
+}
