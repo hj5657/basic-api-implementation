1851fdb79b692c4cdedc5dcf0a0320919e5c498c
hejie <565703319@qq.com>
2020-09-20 16:42:05 +0800

    [hejie]: finish demo get vote record

--- src/main/java/com/thoughtworks/rslist/repository/VoteRepository.java
+++ src/main/java/com/thoughtworks/rslist/repository/VoteRepository.java
@@ -1,14 +1,20 @@
 package com.thoughtworks.rslist.repository;
 
 import com.thoughtworks.rslist.po.VotePo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 /**
  * Create by 木水 on 2020/9/20.
  */
-public interface VoteRepository extends CrudRepository<VotePo,Integer> {
+public interface VoteRepository extends PagingAndSortingRepository<VotePo,Integer> {
     @Override
     List<VotePo> findAll();
+
+    @Query(value = "select * from vote where user_id =:userId and rs_event_id =:rsEventId",nativeQuery = true)
+    List<VotePo> findByUserIdAndRsEventId(int userId, int rsEventId, Pageable pageable);
 }
--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -1,19 +1,88 @@
 package com.thoughtworks.rslist.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.po.RsEventPo;
+import com.thoughtworks.rslist.po.UserPo;
+import com.thoughtworks.rslist.po.VotePo;
+import com.thoughtworks.rslist.repository.RsEventRepository;
+import com.thoughtworks.rslist.repository.UserRepository;
+import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class VoteControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RsEventRepository rsEventRepository;
+    @Autowired
+    VoteRepository voteRepository;
+    UserPo saveUser;
+    RsEventPo rsEventPo;
+
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper();
+        voteRepository.deleteAll();
+        rsEventRepository.deleteAll();
+        userRepository.deleteAll();
+        saveUser = UserPo.builder().age(22).email("hj@c").gender("male").phone("13599999999")
+                .userName("hejie").build();
+        saveUser = userRepository.save(saveUser);
+        rsEventPo = RsEventPo.builder().eventName("基金涨了").keyWord("经济").voteNum(0).build();
+        rsEventPo = rsEventRepository.save(rsEventPo);
+    }
+
+    @Test
+    void should_get_vote_record() throws Exception {
+        for (int i = 0; i < 8; i++) {
+            VotePo votePo = VotePo.builder().userPo(saveUser).rsEventPo(rsEventPo)
+                    .time(LocalDateTime.now()).num(i + 1).build();
+            voteRepository.save(votePo);
+        }
+
+        mockMvc.perform(get("/voteRecord").param("userId", String.valueOf(saveUser.getId()))
+                .param("rsEventId", String.valueOf(rsEventPo.getId()))
+                .param("pageIndex",String.valueOf(1)))
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andExpect(jsonPath("$[0].userId", is(saveUser.getId())))
+                .andExpect(jsonPath("$[0].rsEventId", is(rsEventPo.getId())))
+                .andExpect(jsonPath("$[0].voteNum", is(1)))
+                .andExpect(jsonPath("$[1].voteNum", is(2)))
+                .andExpect(jsonPath("$[2].voteNum", is(3)))
+                .andExpect(jsonPath("$[3].voteNum", is(4)))
+                .andExpect(jsonPath("$[4].voteNum", is(5)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/voteRecord").param("userId", String.valueOf(saveUser.getId()))
+                .param("rsEventId", String.valueOf(rsEventPo.getId()))
+                .param("pageIndex",String.valueOf(2)))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].userId", is(saveUser.getId())))
+                .andExpect(jsonPath("$[0].rsEventId", is(rsEventPo.getId())))
+                .andExpect(jsonPath("$[0].voteNum", is(6)))
+                .andExpect(jsonPath("$[1].voteNum", is(7)))
+                .andExpect(jsonPath("$[2].voteNum", is(8)))
+                .andExpect(status().isOk());
     }
 }
